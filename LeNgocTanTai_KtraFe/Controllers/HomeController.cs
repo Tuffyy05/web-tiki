03348b554c5b1fe13e8684e56427c5bd5c419a7e
Tuffyy05 <ltai75652@gmail.com>
2024-10-07 22:53:02 +0700

    Tantai

---
+++ LeNgocTanTai_KtraFe/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeNgocTanTai_KtraFe.Controllers
+{
+    public class HomeController : Controller
+    {
+   
+        public ActionResult Tantai()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ LeNgocTanTai_KtraFe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeNgocTanTai_KtraFe.MvcApplication" Language="C#" %>
